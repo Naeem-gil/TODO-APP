82566f21fcdd22ba857b5c1368294e7781497f79
Naeem Gill <naeemgill27sep@gmail.com>
2026-04-01 10:17:34 +0500

    Completed my task

--- todo.js
+++ todo.js
@@ -1,5 +1,13 @@
- function myfunction(){
-    const div = document.getElementById("todo-list");
-    div.append=document.getElementById("todo-input").value;
-   
+function myfunction() {
+    const div = document.getElementById("todo-input").value;
+    const element = document.createElement("li");
+    element.textContent = div + " ";
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = function () {
+        element.remove();
+    };
+    element.appendChild(deleteButton);
+
+    document.getElementById("todo-list").appendChild(element);
 }
